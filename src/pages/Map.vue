5fefb74f16cafa40a025c29f888acb9f4deb7c05
alejandra.zuleta <alejandra.zuleta@sofka.com.co>
2022-06-25 19:53:06 -0500

    Se añaden los polígonos independientes y por color

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -14,12 +14,17 @@
       <l-geo-json
         v-if="showGeoJsonMedellin"
         :geojson="geojsonmedellin"
-        :options="optionsGeoJson"
+        :options="options"
       />
 
-      <l-geo-json
-        :geojson="geojson"
-        :options="optionsGeoJson"
+      <l-polygon
+        v-for="(value,key) in polygons"
+        :key="key"
+        :lat-lngs="value.coordinates"
+        :color="value.color"
+        :fill="true"
+        :fillOpacity="0.5"
+        :fillColor="value.color"
       />
 
     </l-map>
@@ -31,7 +36,8 @@
 import {
   LMap,
   LTileLayer,
-  LGeoJson
+  LGeoJson,
+  LPolygon
 } from '@vue-leaflet/vue-leaflet'
 import 'leaflet/dist/leaflet.css'
 import jsonMedellin from 'src/geojsondata/medellin.json'
@@ -42,17 +48,20 @@ export default {
   components: {
     LMap,
     LTileLayer,
-    LGeoJson
+    LGeoJson,
+    LPolygon
   },
   data () {
     return {
       zoom: 11,
       center: [6.244203, -75.581215],
-      geojsonmedellin: jsonMedellin,
+      geojsonmedellin: null,
       geojson: null,
       optionsGeoJson: {
       },
-      showGeoJsonMedellin: false
+      showGeoJsonMedellin: false,
+      polygons: null,
+      latlongs: null
     }
   },
   methods: {
@@ -64,6 +73,10 @@ export default {
     // const response = await fetch("https://rawgit.com/gregoiredavid/france-geojson/master/regions/pays-de-la-loire/communes-pays-de-la-loire.geojson")
     // const data = await response.json();
     this.geojson = jsonHexagonos
+    this.geojsonmedellin = jsonMedellin
+    this.latlongs = jsonHexagonos.map(a => a.geometry.coordinates[0]).map(polygon => polygon.map(a => [a[1], a[0]]))
+    this.polygons = jsonHexagonos.map(a => { return { coordinates: a.geometry.coordinates[0], color: a.properties.color } }).map(polygon => { return { color: polygon.color, coordinates: polygon.coordinates.map(a => [a[1], a[0]]) } })
+    this.log(this.polygons)
   }
 }
 </script>
